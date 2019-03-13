a74ea99dedd659510d926a633881e6286421a55f
David Leitner <david@leitner.io>
2019-03-13 22:22:54 +0100

    Initial Slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { SlideDefinition } from './slider/slider.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div class="center">
+      <app-slider
+        [sliderDirection]="'vertical'"
+        [navigationPosition]="'bottom'"
+        [speed]="3000"
+        [slides]="slides"
+        [width]="800"
+        [height]="600"
+      ></app-slider>
+    </div>
+  `,
+  styles: [
+    `
+      .center {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'ngTraining-content-projection';
+  slides: SlideDefinition[] = [
+    {
+      backgroundImage: 'assets/images/schoenbrunn.png',
+      caption: 'Schönbrunn Palace',
+      description: 'A former imperial summer residence.'
+    },
+    {
+      backgroundImage: 'assets/images/stephansdom.png',
+      caption: "St. Stephen's Cathedral",
+      description: 'One of the most iconic buildings in Vienna.'
+    },
+    {
+      backgroundImage: 'assets/images/riesenrad.png',
+      caption: 'Wiener Riesenrad',
+      description: "Was the world's tallest extant Ferris wheel till 1985."
+    }
+  ];
 }
---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-slider',
+  template: `
+    <div class="dot-container" *ngIf="navigationPosition === 'top'">
+      <div *ngFor="let slide of slides" class="dot" (click)="goToSlide(slide)"></div>
+    </div>
+    <div class="slider-container" [ngStyle]="{ 'width.px': width, 'height.px': height }">
+      <div
+        class="screen"
+        [ngStyle]="{
+          'margin-top.px': offsetY,
+          'margin-left.px': offsetX,
+          'width.px': screenWidth,
+          'flex-direction': flexDirection
+        }"
+      >
+        <div
+          class="slide"
+          *ngFor="let slide of slides"
+          [ngStyle]="{
+            'width.px': width,
+            'height.px': height,
+            'background-image': 'url(' + slide.backgroundImage + ')'
+          }"
+        >
+          <div class="caption">
+            <div class="title">{{ slide.caption }}</div>
+            <div class="description">{{ slide.description }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="dot-container" *ngIf="navigationPosition === 'bottom'">
+      <div *ngFor="let slide of slides" class="dot" (click)="goToSlide(slide)"></div>
+    </div>
+  `,
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit, OnDestroy {
+  @Input()
+  slides: SlideDefinition[];
+  @Input()
+  width: number;
+  @Input()
+  height: number;
+  @Input()
+  speed: number;
+  @Input()
+  sliderDirection: 'vertical' | 'horizontal' = 'horizontal';
+  @Input()
+  navigationPosition: 'top' | 'bottom' = 'bottom';
+
+  public offsetY: number;
+  public offsetX: number;
+  private intervalSubscriber: any;
+
+  public get screenWidth(): number {
+    return this.sliderDirection === 'horizontal' ? this.slides.length * this.width : this.width;
+  }
+
+  public get flexDirection(): string {
+    return this.sliderDirection === 'horizontal' ? 'row' : 'column';
+  }
+
+  ngOnInit(): void {
+    this.intervalSubscriber = interval(this.speed).subscribe(val => {
+      const currentSlide = val % this.slides.length;
+      if (this.sliderDirection === 'vertical') {
+        this.offsetY = currentSlide * this.height * -1;
+      } else {
+        this.offsetX = currentSlide * this.width * -1;
+      }
+    });
+  }
+
+  goToSlide(slide: SlideDefinition) {
+    const slideToGoTo = this.slides.indexOf(slide);
+    if (this.sliderDirection === 'vertical') {
+      this.offsetY = slideToGoTo * this.height * -1;
+    } else {
+      this.offsetX = slideToGoTo * this.width * -1;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscriber.unsubscribe();
+  }
+}
+
+export interface SlideDefinition {
+  backgroundImage: string;
+  caption: string;
+  description: string;
+}
