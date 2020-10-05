af344c0d1ed7e4a40f4a8110e4500d742018a742
David Leitner <david@leitner.io>
2020-10-05 08:57:01 +0200

    Update to Angular 10

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngTraining-content-projection!');
+    expect(page.getTitleText()).toEqual('ngTraining-content-projection app is running!');
   });
 
   afterEach(async () => {
